f660fd678e2f5fd19b5bf33bdd711fe6255c4fe2
Clare55 <clarnic@hotmail.com>
2017-12-15 16:08:48 +0000

    Update 1 - GetTweets.R

--- Code/R-Code/1 - GetTweets.R
+++ Code/R-Code/1 - GetTweets.R
@@ -34,8 +34,8 @@ library(ROAuth)
 requestURL <- "https://api.twitter.com/oauth/request_token"
 accessURL <- "https://api.twitter.com/oauth/access_token"
 authURL <- "https://api.twitter.com/oauth/authorize"
-consumerKey <- "UMHqEEONpSIiuMD1Dt3jxMpeR"
-consumerSecret <- "kX8WqbnGwRlzlTlxWVoPkcJQQC4oieJUbzoMYObES1K3SawZIw"
+consumerKey <- "XXXXXXX"
+consumerSecret <- "XXXXXXXXX"
 my_oauth <- OAuthFactory$new(consumerKey = consumerKey, consumerSecret = consumerSecret, 
                              requestURL = requestURL, accessURL = accessURL, authURL = authURL)
 my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
